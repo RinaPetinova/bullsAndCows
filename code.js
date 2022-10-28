9326a991087e83a05a63977c90728359f61ce55d
RinaPetinova <90608011+RinaPetinova@users.noreply.github.com>
2022-10-28 22:26:08 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,110 @@
+let answer = [];                                    //Генерируется в функции generateAnswer
+let guessedArr=[];                                  //Генерируется в функции check
+let divOut = document.querySelector('.out');        //Получаем блок вывода
+let input=document.querySelector('.i-1');           //Получаем блок ввода
+let button =  document.querySelector('.b-1');
+generateAnswer();                                   //Генерация числа из 4х неповторяющихся случайных цифр
+console.log(answer); 
+let counter = 0;
+
+function check() {
+    counter++;
+    let byki=0;
+    let korovi=0;
+    
+    //Создается массив из строки, полученной в input
+    guessedArr=Array.from(String(input.value), Number);
+    // console.log(guessedArr);
+
+    //проверка на не-число
+    if (isNaN(input.value)){
+        divOut.innerHTML = 'Введите число! <br>';
+        return;
+    //проверка на количество введенных символов
+    } else if (guessedArr.length!==4) {
+            divOut.innerHTML += 'Введите число из 4х цифр! <br>';
+            return;
+    } 
+    
+    //проверка на повторяемость цифр в числе
+    for (let i=0; i<guessedArr.length; i++) {
+        for (let k = 0; k<i; k++) {
+            if (guessedArr[i]==guessedArr[k]) {
+                divOut.innerHTML += 'Цифры не должны повторяться! <br>';
+                return;
+            }
+        }
+    }
+
+    //перебор массива и вычисление количества быков и коров
+    for (let i=0; i<=3;i++){
+        if(guessedArr[i]===answer[i]){
+            byki++;
+        } 
+
+        for (let k=0; k<=3; k++) {
+            if (guessedArr[k]===answer[i]){
+                    korovi++;
+            }
+        }
+    }
+   
+    //вывод в div резульата операции
+    divOut.innerHTML += `${input.value}   Быков: ${byki}, Коров: ${korovi} <br>`;
+
+    //в случае выигрыша выводится сообщение
+    if (byki===4 && korovi === 4) {
+        divOut.innerHTML += `You won in ${counter} attempts! <br>`;
+        //и вызывается функция, которая создает новый div с сообщением и перегружает страницу
+        again();
+        button.setAttribute("disabled", "disabled");
+        }
+    
+        document.querySelector('.i-1').value="";
+        
+}
+
+button.addEventListener('click', check);
+button.addEventListener('touch', check);
+input.addEventListener("keypress", function(event){
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      check();
+      event.preventDefault();
+    //   // Trigger the button element with a click
+    //   document.getElementById("myBtn").click();
+    }
+    
+});
+
+
+
+
+function again() {
+    let divAgain = document.createElement('input');
+    let mainDiv = document.querySelector('.container');
+    mainDiv.appendChild(divAgain);
+    divAgain.type = 'button';
+    divAgain.value = 'Начать игру заново';
+    divAgain.setAttribute('class', "btn btn-info")
+    divAgain.addEventListener("click", reloadPage);
+    // setTimeout(reloadPage, 6000);
+}
+    
+function reloadPage() {
+   location.reload();
+}
+
+function generateAnswer() {
+    let num;
+    
+     for (let i = 0; i<= 3; i++) {
+        num = Math.floor(Math.random()*10);
+        if (answer.includes(num)) {
+            i--;
+        } else {
+            answer.push(num);
+        }
+     }
+     return answer;
+}
